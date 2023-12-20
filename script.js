7d7c2aafaccaaf2cf514260461b93945af47558c
asad200-OK <asadullakhan0098@gmail.com>
2023-12-20 14:14:29 +0530

    Add playRound function

--- script.js
+++ script.js
@@ -24,3 +24,34 @@ function getPlayerChoice() {
     return userChoice.toLowerCase();
 }
 
+/* Write a function that plays a single round of Rock Paper Scissors and either replays the round if there's a tie or returns who won*/
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return playRound(getPlayerChoice(), getComputerChoice());
+    }
+    if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Paper beats Rock';
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock';
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    }
+    else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    }
+    else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats Paper';
+    }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
+
+
+
+
